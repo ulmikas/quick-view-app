2d4e72881f5831e4aa007e2540d676e73d1729a1
kenway <kenway@ecwid.com>
2017-05-18 00:33:42 +0400

    add redux

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="quick-view">
+        <div className="quick-view__picture">
+          <img src={logo} className="quick-view__image" alt="logo" />
+          <h2>Welcome React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="quick-view__info">
+          info
+        </div>
+
+        <div className="quick-view__description">
+          <p>
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
       </div>
     );
   }
